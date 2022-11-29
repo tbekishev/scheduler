77416456ee746e6684b37d6fd4d1f16e612cfc10
Tuigun Bekishev <tbekishev@gmail.com>
2022-11-29 19:50:46 -0400

    fix DaylistItem styling

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,8 +1,13 @@
 import React from "react";
+import "components/DayListItem.scss";
+import classNames from "classnames";
 
 export default function DayListItem(props) {
+  let buttonClass = classNames("day-list__item", {
+    "--selected" : props.selected,
+    "--full" : !props.spots});
   return (
-    <li onClick={()=>props.setDay(props.name)}>
+    <li className={buttonClass.replace(/\s/g, '')} onClick={()=>props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2> 
       <h3 className="text--light">{props.spots} spots remaining</h3>
     </li>
