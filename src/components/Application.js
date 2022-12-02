dcabba4c35b1a293a4da0561a53098136c8298d0
Tuigun Bekishev <tbekishev@gmail.com>
2022-12-02 11:06:32 -0400

    add useEffect and axios to fetch data from localhost:8081/api/days

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,30 +1,59 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import "components/Application.scss";
 import DayList from "./DayList";
+import "components/Appointment"
+import Appointment from "components/Appointment";
+import axios  from "axios";
 
-const days = [
-  {
+const appointments = {
+  "1": {
     id: 1,
-    name: "Monday",
-    spots: 2,
+    time: "12pm",
   },
-  {
+  "2": {
     id: 2,
-    name: "Tuesday",
-    spots: 5,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer:{
+        id: 3,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
   },
-  {
+  "3": {
     id: 3,
-    name: "Wednesday",
-    spots: 0,
+    time: "2pm",
   },
-];
-
+  "4": {
+    id: 4,
+    time: "3pm",
+    interview: {
+      student: "Archie Andrews",
+      interviewer:{
+        id: 4,
+        name: "Cohana Roy",
+        avatar: "https://i.imgur.com/FK8V841.jpg",
+      }
+    }
+  },
+  "5": {
+    id: 5,
+    time: "4pm",
+  }
+};
 
 
 export default function Application(props) {
-  const [day, setDay] = useState('Monday');
+  const [days, setDays] = useState([]);
+  useEffect(() => {
+    axios.get("http://localhost:8001/api/days")
+    .then(response => {
+      setDays(response.data);
+    })
+  }, []);
   return (
     <main className="layout">
       <section className="sidebar">
@@ -37,8 +66,8 @@ export default function Application(props) {
         <nav className="sidebar__menu">
           <DayList
             days={days}
-            value={day}
-            onChange={setDay}
+            value={"Monday"}
+            onChange={setDays}
           />
         </nav>
         <img
@@ -49,7 +78,12 @@ export default function Application(props) {
         {/* Replace this with the sidebar elements during the "Project Setup & Familiarity" activity. */}
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        {Object.values(appointments).map((appointment) => 
+          <Appointment
+            key={appointment.id}
+            {...appointment}
+          />
+        )}
       </section>
     </main>
   );
